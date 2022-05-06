2be093ee51443979b2fc3b7d86d3741d58e90400
LayaneB <layanejuvito@gmail.com>
2022-05-06 15:51:04 -0300

    Adicionando a Paginação

--- pokedex/src/AppStyle.js
+++ pokedex/src/AppStyle.js
@@ -7,4 +7,16 @@ export const GlobalStyle = createGlobalStyle`
     padding: 0;
     box-sizing: border-box;
 }
+*::-webkit-scrollbar {
+    width: 4px;
+    height: 5px;
+    
+  }
+*::-webkit-scrollbar-thumb {
+      background-color: #6C6C6C;
+      border-radius: 2px;
+  }
+*::-webkit-scrollbar-track {
+      background-color:#DFDFDF;
+  }
 `
--- pokedex/src/pages/Home/style.js
+++ pokedex/src/pages/Home/style.js
@@ -4,14 +4,5 @@ export const HomeContainer = styled.div`
     display: flex;
     flex-direction: column;
     align-items: center;
-    width: 100vw; 
-`
-
-export const PokeCards = styled.div`
-   display: grid;
-    grid-template-columns: repeat(3, 18rem);
-    grid-template-rows: auto;
-    row-gap: 1%;
-    column-gap: 2%;
-    /* align-self: center; */
+    max-width: 100vw; 
 `
