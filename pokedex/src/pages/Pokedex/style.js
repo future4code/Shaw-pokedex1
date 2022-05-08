083971266e9b1ea9213af243451395319105a9dc
LayaneB <layanejuvito@gmail.com>
2022-05-08 10:53:02 -0300

    correção largura máxima pokédex

--- pokedex/src/pages/Pokedex/style.js
+++ pokedex/src/pages/Pokedex/style.js
@@ -4,7 +4,7 @@ export const PokedexMain = styled.div`
     display: flex;
     flex-direction: column;
     align-items: center;
-    width: 100vw; 
+    max-width: 100vw; 
     min-height: 80vh;
 `
 
