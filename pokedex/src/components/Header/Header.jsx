d8875290b2ec782a5364385a3de35edfda27ff7d
LayaneB <layanejuvito@gmail.com>
2022-05-08 10:39:33 -0300

    correção botão de voltar do header

--- pokedex/src/components/Header/Header.jsx
+++ pokedex/src/components/Header/Header.jsx
@@ -42,55 +42,6 @@ const Header = (props) => {
                     </DivDex>
                 </HeaderDiv>
                 : 
-
-                props.page === 'details' ?
-                <HeaderDiv>
-                    {
-                        pageBefore === 'pokedex' ?
-
-                        <Tooltip hasArrow bg='gray.400' color={'black'} label='Voltar' placement='top-end'>
-                            <Button onClick={() => goToPokedexPage(navigate)}>
-                                <TiArrowBack 
-                                    size={50} 
-                                    color={'#E41C24'} 
-                                    style={{position: "absolute", top: "14px"}} 
-                                />
-                            </Button>
-                        </Tooltip>
-                        :
-                        <Tooltip hasArrow bg='gray.400' color={'black'} label='Voltar' placement='top-end'>
-                            <Button onClick={() => goToHomePage(navigate)}>
-                                <TiArrowBack 
-                                    size={50} 
-                                    color={'#E41C24'} 
-                                    style={{position: "absolute", top: "14px"}} 
-                                />
-                            </Button>
-                        </Tooltip>
-                    }
-
-                    <DivLogoOthers>
-                        <Tooltip hasArrow bg='gray.400' color={'black'} label='Início'>
-                            <Button onClick={() => goToHomePage(navigate)}>
-                                <Img src={LogoDex} alt="Botão para página inicial" />
-                            </Button>
-                        </Tooltip>
-                    </DivLogoOthers>
-                    
-                    <DivDex>
-                        <Tooltip hasArrow bg='gray.400' color={'black'} label='Batalha'>
-                            <Button onClick={() => goToBattlePage(navigate)}>
-                                <Img src={Battle} alt="Botão para página de batalha" />
-                            </Button>
-                        </Tooltip>
-                        <Tooltip hasArrow bg='gray.400' color={'black'} label='Pokédex'>
-                            <Button onClick={() => goToPokedexPage(navigate)}>
-                                <Img src={Pokedex} alt="Botão para página de Pokédex" />
-                            </Button>
-                        </Tooltip>
-                    </DivDex>
-                </HeaderDiv>
-                :
                 <HeaderDiv>
                     
                     <Tooltip hasArrow bg='gray.400' color={'black'} label='Voltar' placement='top-end'>
